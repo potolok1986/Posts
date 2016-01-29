54df72dc0da3e7c271e3b308633536e49df63aad
Александр <potolok1986@gmail.com>
2016-01-29 23:49:25 +0600

    commit

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,36 @@
-var posts = [{"text":"первая тестовая заметка","date":"18.00"}];
+var MyLocalStorage = {
+	storage:[],
+	_storageName : "postsStorage",
+	responseLocalStorage : function () {
+		this.storage = [];
+		if(localStorage.getItem(this._storageName)){
+			this.storage = (JSON.parse(localStorage.getItem(this._storageName)));
+		}
+		return this.storage;
+	},
+	addToStorage: function($scope){
+		var newPost = {
+			"text": $scope.postText,
+			date: new Date().getMinutes()
+		};
+
+		$scope.posts.push(newPost);
+		this.storage.push(newPost);
+		localStorage.setItem(this._storageName,angular.toJson(this.storage));
+	}
+
+};
+
 postsCtrl = function($scope){
 	$scope.maxLength = 15;
-	$scope.minLength = 2;
-	$scope.posts = posts.reverse();
+	$scope.minLength = 1;
+	$scope.posts = MyLocalStorage.responseLocalStorage() || [];
+	MyLocalStorage.responseLocalStorage();
 	$scope.addPost = function () {
-		$scope.posts.push({
-			"text":this.postText,
-			"data": new Date().getHours()
-		});
+		MyLocalStorage.addToStorage($scope);
 		this.postText = "";
-
 	};
 	$scope.deletePost = function(){
-		console.log(this.$index);
 		$scope.posts.splice(this.$index,1);
 	}
 
@@ -20,3 +38,4 @@ postsCtrl = function($scope){
 angular
 	.module("postsApp",[])
 	.controller("PostsController",postsCtrl);
+
