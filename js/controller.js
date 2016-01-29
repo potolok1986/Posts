a36fdf82d833429e3026b5d9e226b02a0d5e90f4
Александр <potolok1986@gmail.com>
2016-01-29 17:17:35 +0600

    ангуляр

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,19 @@
+var posts = [{"text":"первая тестовая заметка","date":"18.00"}];
+
+
+
+
+
 postsCtrl = function($scope){
-	$scope.maxLength = 150;
-console.log($scope);
+	$scope.maxLength = 15;
+	$scope.minLength = 2;
+	$scope.posts = posts;
+	$scope.addPost = function () {
+		this.postText = "";
+
+		console.log(this);
+	};
+
 };
 angular
 	.module("postsApp",[])
