3387340adfb9a826e765c63d758780b719689ead
Александр <potolok1986@gmail.com>
2016-01-29 21:08:46 +0600

    commit

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,20 @@
 var posts = [{"text":"первая тестовая заметка","date":"18.00"}];
-
-
-
-
-
 postsCtrl = function($scope){
 	$scope.maxLength = 15;
 	$scope.minLength = 2;
-	$scope.posts = posts;
+	$scope.posts = posts.reverse();
 	$scope.addPost = function () {
+		$scope.posts.push({
+			"text":this.postText,
+			"data": new Date().getHours()
+		});
 		this.postText = "";
 
-		console.log(this);
 	};
+	$scope.deletePost = function(){
+		console.log(this.$index);
+		$scope.posts.splice(this.$index,1);
+	}
 
 };
 angular
